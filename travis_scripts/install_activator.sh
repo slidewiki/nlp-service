ea78f608f6701693d88995f401052a8ddf495628
Roy Meissner <meissner@informatik.uni-leipzig.de>
2017-02-22 13:58:32 +0100

    Fixed install script

--- travis_scripts/install_activator.sh
+++ travis_scripts/install_activator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -L https://downloads.typesafe.com/typesafe-activator/$ACTIVATOR_VERSION/typesafe-activator-$ACTIVATOR_VERSION-minimal.zip > activator.zip
-unzip -uo activator.zip -d ./activator/
-chmod +x ./activator/bin/activator
+unzip -uo activator.zip
+chmod +x ./activator-$ACTIVATOR_VERSION-minimal/bin/activator
 
-export $PATH:$PWD/activator/bin/
+export PATH=$PATH:$PWD/activator-$ACTIVATOR_VERSION-minimal/bin
