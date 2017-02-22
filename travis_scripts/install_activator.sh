660f9aa721621f2c922396c7ef74472e40cd5309
Roy Meissner <meissner@informatik.uni-leipzig.de>
2017-02-22 14:41:20 +0100

    Another approach for a fix

--- travis_scripts/install_activator.sh
+++ travis_scripts/install_activator.sh
@@ -4,4 +4,5 @@ curl -L https://downloads.typesafe.com/typesafe-activator/$ACTIVATOR_VERSION/typ
 unzip -uo activator.zip
 chmod +x ./activator-$ACTIVATOR_VERSION-minimal/bin/activator
 
-export PATH=$PATH:$PWD/activator-$ACTIVATOR_VERSION-minimal/bin
+mkdir ~/.activator
+mv ./activator-$ACTIVATOR_VERSION-minimal/* ~/.activator/
