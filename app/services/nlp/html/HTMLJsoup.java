eb9e5732cb254ed9af3aedc2cef625a87f0e1bea
Antje Schlaf <antje.schlaf@informatik.uni-leipzig.de>
2017-03-10 10:51:33 +0100

    HTMLJsoup: added further parsing, because parsing just once wasn't enough to get text from html (problem occured when processing slide content of old platform)

--- app/services/nlp/html/HTMLJsoup.java
+++ app/services/nlp/html/HTMLJsoup.java
@@ -6,7 +6,11 @@ public class HTMLJsoup implements IHtmlToText{
 
 	@Override
 	public String getText(String html) {
-		return Jsoup.parse(html).text();
+		String text = Jsoup.parse(html).text();
+		// do this parsing again because for old platform e.g. for deck id 149, slide id 2380-3 the text wasn't correctly extracted
+		String result = Jsoup.parse(text).text();
+		
+		return result;
 	}
 
 }
