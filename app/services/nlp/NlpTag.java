a5b678298902279225a0e1ecadc761dea0198a09
Antje Schlaf <antje.schlaf@informatik.uni-leipzig.de>
2017-04-11 09:17:00 +0200

    NLPTag: added tokenSpanBegin and tokenSpanEnd (needed for retrieving distinct NEs - to identify same NEs retrieved by distinct NER sources)

--- app/services/nlp/NlpTag.java
+++ app/services/nlp/NlpTag.java
@@ -3,6 +3,9 @@ package services.nlp;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import opennlp.tools.util.Span;
 import services.nlp.ner.NER_OpenNLP;
 
@@ -12,25 +15,44 @@ public class NlpTag {
 	private String type;
 	private String sourceName;
 	private double probability;
+	private int tokenSpanBegin;
+	private int tokenSpanEnd;
 	private String link;
 
 	
-	public NlpTag(String name, String type, String sourceName, double probability, String link) {
+	/**
+	 * 
+	 * @param name
+	 * @param type
+	 * @param sourceName
+	 * @param probability
+	 * @param spanBegin the start of the NER span (the token index, not the char index) 
+	 * @param spanEnd the end of the NER span (the token index, not the char index), which is +1 more than the last element in the span.
+	 * @param link
+	 */
+	@JsonCreator
+	public NlpTag(@JsonProperty("name") String name, @JsonProperty("type") String type, @JsonProperty("sourceName") String sourceName, @JsonProperty("probability") double probability, @JsonProperty("tokenSpanBegin") int tokenSpanBegin, @JsonProperty("tokenSpanEnd") int tokenSpanEnd, @JsonProperty("link") String link) {
 		super();
 		this.name = name;
 		this.type = type;
 		this.sourceName = sourceName;
 		this.probability = probability;
+		this.tokenSpanBegin = tokenSpanBegin;
+		this.tokenSpanEnd = tokenSpanEnd;
 		this.link = link;
 	}
 
 
+	
 
 	public NlpTag(Span span, String[] tokens, String sourceName){
 		this.name = NER_OpenNLP.getTokenStringFromSpan(span, tokens);
 		this.type = span.getType();
 		this.probability = span.getProb();
 		this.sourceName = sourceName;
+		this.tokenSpanBegin = span.getStart();
+		this.tokenSpanEnd = span.getEnd();
+		this.link = "";
 	}
 	
 	public static List<NlpTag> fromSpans(Span[] spans, String[] tokens, String sourceName){
@@ -100,5 +122,24 @@ public class NlpTag {
 		this.link = link;
 	}
 
+
+	/**
+	 * Returns the begin of the NER span (the token index, not the char index)
+	 * @return
+	 */
+	public int getTokenSpanBegin() {
+		return tokenSpanBegin;
+	}
+
+
+	/**
+	 * Returns the end of the NER span (the token index, not the char index), which is +1 more than the last element in the span.
+	 * @return
+	 */
+	public int getTokenSpanEnd() {
+		return tokenSpanEnd;
+	}
+
+	
 
 }
