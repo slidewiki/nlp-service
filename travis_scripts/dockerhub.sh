95f5b81c73b24e82653d2ebf35381190854ba0a7
Antje Schlaf <antje.schlaf@informatik.uni-leipzig.de>
2017-11-02 18:00:17 +0100

    added brackets

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-if [ $TRAVIS_TAG =~ ^[0-9]+(\.[0-9]+)+$ ]
+if [[ $TRAVIS_TAG =~ ^[0-9]+(\.[0-9]+)+$ ]]
 then
 	docker build -t slidewiki/nlpservice:$TRAVIS_TAG ./
 	docker push slidewiki/nlpservice:$TRAVIS_TAG
